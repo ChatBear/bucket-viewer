440d1af34b3afc1a6a4b7a60503a59ab07b24d58
ChatBear <adamaly.shiraz@outlook.fr>
2025-08-26 17:34:05 +0530

    first commit - backend

---
+++ bucket-viewer-backend/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+app = FastAPI() 
+
+def main():
+    print("Hello from bucket-viewer-backend!")
+
+
+if __name__ == "__main__":
+    print("je suis un didier en espérons que le monde m'appartienne")
+    main()
