a88cd6d62f03a5fa9a9fa63c14bf531683633308
ChatBear <adamaly.shiraz@outlook.fr>
2025-09-18 18:25:32 +0700

    fix

--- infrastructure/modules/oicd_resource/main.tf
+++ infrastructure/modules/oicd_resource/main.tf
@@ -28,154 +28,160 @@ resource "aws_iam_role" "github_actions" {
   })
 }
 
-# Policy for ECR operations
-resource "aws_iam_policy" "ecr_policy" {
-  name        = "github-actions-ecr-policy"
-  description = "Policy for GitHub Actions to push to ECR"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "ecr:GetAuthorizationToken",
-          "ecr:BatchCheckLayerAvailability",
-          "ecr:GetDownloadUrlForLayer",
-          "ecr:BatchGetImage",
-          "ecr:InitiateLayerUpload",
-          "ecr:UploadLayerPart",
-          "ecr:CompleteLayerUpload",
-          "ecr:PutImage"
-        ]
-        Resource = "*"
-      }
-    ]
-  })
-}
-
-# Policy for Lambda operations
-resource "aws_iam_policy" "lambda_policy" {
-  name        = "github-actions-lambda-policy"
-  description = "Policy for GitHub Actions to deploy Lambda functions"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "lambda:UpdateFunctionCode",
-          "lambda:UpdateFunctionConfiguration",
-          "lambda:GetFunction",
-          "lambda:CreateFunction"
-        ]
-        Resource = "*"
-      }
-    ]
-  })
-}
-
-# Policy for S3 operations
-resource "aws_iam_policy" "s3_policy" {
-  name        = "github-actions-s3-policy"
-  description = "Policy for GitHub Actions to access S3 buckets"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "s3:GetObject",
-          "s3:PutObject",
-          "s3:DeleteObject",
-          "s3:ListBucket",
-          "s3:GetBucketLocation",
-          "s3:GetBucketVersioning",
-          "s3:GetBucketPolicy",
-        ]
-        Resource = "*"
-      }
-    ]
-  })
-}
-
-# Policy for CloudFront operations
-resource "aws_iam_policy" "cloudfront_policy" {
-  name        = "github-actions-cloudfront-policy"
-  description = "Policy for GitHub Actions to invalidate CloudFront cache"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "cloudfront:CreateInvalidation",
-          "cloudfront:GetInvalidation",
-          "cloudfront:ListInvalidations",
-          "cloudfront:GetDistribution",
-          "cloudfront:ListDistributions"
-        ]
-        Resource = "*"
-      }
-    ]
-  })
-}
-
-# Policy for DynamoDB operations
-resource "aws_iam_policy" "dynamodb_policy" {
-  name        = "github-actions-dynamodb-policy"
-  description = "Policy for GitHub Actions to access DynamoDB"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "dynamodb:GetItem",
-          "dynamodb:PutItem",
-          "dynamodb:UpdateItem",
-          "dynamodb:DeleteItem",
-          "dynamodb:Scan",
-          "dynamodb:Query",
-          "dynamodb:BatchGetItem",
-          "dynamodb:BatchWriteItem",
-          "dynamodb:DescribeTable"
-        ]
-        Resource = "*"
-      }
-    ]
-  })
-}
-
-
-
-# Attache the policies to the role
-resource "aws_iam_role_policy_attachment" "ecr_policy_attachment" {
-  policy_arn = aws_iam_policy.ecr_policy.arn
+# Attach AWS managed AdminAccess policy ---- TO REMOVE AFTER LAZYNESS
+resource "aws_iam_role_policy_attachment" "admin_access" {
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
   role       = aws_iam_role.github_actions.name
 }
 
-resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
-  policy_arn = aws_iam_policy.lambda_policy.arn
-  role       = aws_iam_role.github_actions.name
-}
+# # Policy for ECR operations
+# resource "aws_iam_policy" "ecr_policy" {
+#   name        = "github-actions-ecr-policy"
+#   description = "Policy for GitHub Actions to push to ECR"
 
-resource "aws_iam_role_policy_attachment" "s3_policy_attachment" {
-  policy_arn = aws_iam_policy.s3_policy.arn
-  role       = aws_iam_role.github_actions.name
-}
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Effect = "Allow"
+#         Action = [
+#           "ecr:GetAuthorizationToken",
+#           "ecr:BatchCheckLayerAvailability",
+#           "ecr:GetDownloadUrlForLayer",
+#           "ecr:BatchGetImage",
+#           "ecr:InitiateLayerUpload",
+#           "ecr:UploadLayerPart",
+#           "ecr:CompleteLayerUpload",
+#           "ecr:PutImage"
+#         ]
+#         Resource = "*"
+#       }
+#     ]
+#   })
+# }
 
-resource "aws_iam_role_policy_attachment" "cloudfront_policy_attachment" {
-  policy_arn = aws_iam_policy.cloudfront_policy.arn
-  role       = aws_iam_role.github_actions.name
-}
+# # Policy for Lambda operations
+# resource "aws_iam_policy" "lambda_policy" {
+#   name        = "github-actions-lambda-policy"
+#   description = "Policy for GitHub Actions to deploy Lambda functions"
 
-resource "aws_iam_role_policy_attachment" "dynamodb_policy_attachment" {
-  policy_arn = aws_iam_policy.dynamodb_policy.arn
-  role       = aws_iam_role.github_actions.name
-}
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Effect = "Allow"
+#         Action = [
+#           "lambda:UpdateFunctionCode",
+#           "lambda:UpdateFunctionConfiguration",
+#           "lambda:GetFunction",
+#           "lambda:CreateFunction"
+#         ]
+#         Resource = "*"
+#       }
+#     ]
+#   })
+# }
+
+# # Policy for S3 operations
+# resource "aws_iam_policy" "s3_policy" {
+#   name        = "github-actions-s3-policy"
+#   description = "Policy for GitHub Actions to access S3 buckets"
+
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Effect = "Allow"
+#         Action = [
+#           "s3:GetObject",
+#           "s3:PutObject",
+#           "s3:DeleteObject",
+#           "s3:ListBucket",
+#           "s3:GetBucketLocation",
+#           "s3:GetBucketVersioning",
+#           "s3:GetBucketPolicy",
+#         ]
+#         Resource = "*"
+#       }
+#     ]
+#   })
+# }
+
+# # Policy for CloudFront operations
+# resource "aws_iam_policy" "cloudfront_policy" {
+#   name        = "github-actions-cloudfront-policy"
+#   description = "Policy for GitHub Actions to invalidate CloudFront cache"
+
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Effect = "Allow"
+#         Action = [
+#           "cloudfront:CreateInvalidation",
+#           "cloudfront:GetInvalidation",
+#           "cloudfront:ListInvalidations",
+#           "cloudfront:GetDistribution",
+#           "cloudfront:ListDistributions"
+#         ]
+#         Resource = "*"
+#       }
+#     ]
+#   })
+# }
+
+# # Policy for DynamoDB operations
+# resource "aws_iam_policy" "dynamodb_policy" {
+#   name        = "github-actions-dynamodb-policy"
+#   description = "Policy for GitHub Actions to access DynamoDB"
+
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Effect = "Allow"
+#         Action = [
+#           "dynamodb:GetItem",
+#           "dynamodb:PutItem",
+#           "dynamodb:UpdateItem",
+#           "dynamodb:DeleteItem",
+#           "dynamodb:Scan",
+#           "dynamodb:Query",
+#           "dynamodb:BatchGetItem",
+#           "dynamodb:BatchWriteItem",
+#           "dynamodb:DescribeTable"
+#         ]
+#         Resource = "*"
+#       }
+#     ]
+#   })
+# }
+
+
+
+# # Attache the policies to the role
+# resource "aws_iam_role_policy_attachment" "ecr_policy_attachment" {
+#   policy_arn = aws_iam_policy.ecr_policy.arn
+#   role       = aws_iam_role.github_actions.name
+# }
+
+# resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
+#   policy_arn = aws_iam_policy.lambda_policy.arn
+#   role       = aws_iam_role.github_actions.name
+# }
+
+# resource "aws_iam_role_policy_attachment" "s3_policy_attachment" {
+#   policy_arn = aws_iam_policy.s3_policy.arn
+#   role       = aws_iam_role.github_actions.name
+# }
+
+# resource "aws_iam_role_policy_attachment" "cloudfront_policy_attachment" {
+#   policy_arn = aws_iam_policy.cloudfront_policy.arn
+#   role       = aws_iam_role.github_actions.name
+# }
+
+# resource "aws_iam_role_policy_attachment" "dynamodb_policy_attachment" {
+#   policy_arn = aws_iam_policy.dynamodb_policy.arn
+#   role       = aws_iam_role.github_actions.name
+# }
 
